f829f4233fbf6b1065d2222f5809de1f15e9198d
Kerothan <scottmacd5@gmail.com>
2018-12-20 22:55:30 -0500

    initial core component code
    Added template and inputs to core component.
    Added button to construct drones

--- src/app/game/core/core.component.ts
+++ src/app/game/core/core.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Inventory } from 'src/app/models/inventory';
+import { Core } from 'src/app/models/core';
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { Units } from 'src/app/models/units';
 
 @Component({
   selector: 'app-core',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreComponent implements OnInit {
 
+  @Input() inv: Inventory;
+  @Input() core: Core;
+  @Input() unit: Units;
+
+  droneCost:number = 2;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  buyDroneValid():boolean {
+    if (this.droneCost < this.inv.rawMats) return true;
+    else return false;
+  }
+  buyDrone() {
+    this.inv.rawMats -= this.droneCost;
+    this.unit.drones += 1;
+  }
 }
